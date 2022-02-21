ed27c85c485f7d300650769bdb645b5637445d43
MaxGill03 <98751815+MaxGill03@users.noreply.github.com>
2022-02-21 13:46:43 +0100

    Allan

---
+++ joke bot.py
@@ -0,0 +1,28 @@
+import discord
+import joke_api
+
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('joke'):
+        joke = joke_api.get_joke()
+        print(joke)
+
+        if joke == False:
+            await message.channel.send("Couldn't get joke from API. Try again later.")
+        else:
+            await message.channel.send(joke['setup'] + '\n' + joke['punchline'])
+   
+
+client.run('OTM1MTM4MDU3MzI4NDI3MDM5.Ye6RLg.F-YxNq36KNj7LCSYRra3uxorxZM')
