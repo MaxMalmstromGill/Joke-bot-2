15d9a4537e1d946eb37e191e5babb16e0f05cce3
MaxGill03 <98751815+MaxGill03@users.noreply.github.com>
2022-03-07 12:09:05 +0100

    Update joke bot.py

--- joke bot.py
+++ joke bot.py
@@ -1,28 +1,31 @@
 import discord
-import joke_api
+import requests
+import json
 
 
-client = discord.Client()
+class myClient(discord.Client):
+    async def on_ready(self):
+        print(f'Logged in as {client.user}'.format(client))
 
 
-@client.event
-async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+
+    async def on_message(self, ctx):
+            if ctx.author == client.user:
+                return
+            elif ctx.content.startswith('make me laugh'):
+                await ctx.channel.send('Chuck Norris joke?')
+                res = await client.wait_for('message', check=lambda message: ctx.author == message.author)
+                if res.content.lower() == ('yes'):
+                    async def get_joke(self):
+                        response = requests.get(
+                            'https://api.chucknorris.io/jokes/random')
+                        joke = json.loads(response.text)
+                        return(joke['value'])
+                    the_joke = await get_joke(self)
+                    await ctx.channel.send(the_joke)
+                else:
+                    await ctx.channel.send('no problem!')
 
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('joke'):
-        joke = joke_api.get_joke()
-        print(joke)
-
-        if joke == False:
-            await message.channel.send("Couldn't get joke from API. Try again later.")
-        else:
-            await message.channel.send(joke['setup'] + '\n' + joke['punchline'])
-   
-
-client.run('OTM1MTM4MDU3MzI4NDI3MDM5.Ye6RLg.F-YxNq36KNj7LCSYRra3uxorxZM')
+client = myClient()
+client.run('OTM1MTM4MDU3MzI4NDI3MDM5.Ye6RLg.QK7Hx4Cjp1LAEjGeQwnKb2pe5vQ')
