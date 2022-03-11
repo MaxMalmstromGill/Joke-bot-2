6209b43d7b66a691506001de56d40cd168816cc0
MaxGill03 <98751815+MaxGill03@users.noreply.github.com>
2022-03-11 10:47:34 +0100

    Update joke bot.py

--- joke bot.py
+++ joke bot.py
@@ -2,7 +2,6 @@ import discord
 import requests
 import json
 import random
-import os
 
 
 class myClient(discord.Client):
@@ -14,22 +13,22 @@ class myClient(discord.Client):
     async def on_message(self, ctx):
             if ctx.author == client.user:
                 return
-            elif ctx.content.startswith('make me laugh'):
+            elif ctx.content.startswith('::cn'):
                 await ctx.channel.send('Chuck Norris joke?')
-                res = await client.wait_for('message', check=lambda message: ctx.author == message.author)
-                if res.content.lower() == ('yes'):
+                chuck = await client.wait_for('message', check=lambda message: ctx.author == message.author)
+                if chuck.content.lower() == ('yes'):
                     async def get_joke(self):
-                        response = requests.get(
-                            'https://api.chucknorris.io/jokes/random')
-                        joke = json.loads(response.text)
-                        return(joke['value'])
+                            response = requests.get(
+                                'https://api.chucknorris.io/jokes/random')
+                            joke = json.loads(response.text)
+                            return(joke['value'])
                     the_joke = await get_joke(self)
                     await ctx.channel.send(the_joke)
-                else:
-                    await ctx.channel.send('Yo momma joke?')
-                    momma = await client.wait_for('message', check=lambda message: ctx.author == message.author)
-                    if momma.content.lower() == ('yes'):
-                        async def yomomma(self):
+            elif ctx.content.startswith('::ym'):
+                await ctx.channel.send('Yo momma joke?')
+                momma = await client.wait_for('message', check=lambda message: ctx.author == message.author)
+                if momma.content.lower() == ('yes'):
+                    async def yomomma(self):
                             response = requests.get(
                                 'https://api.yomomma.info/')
                             yo = json.loads(response.text)
@@ -40,23 +39,23 @@ class myClient(discord.Client):
 
     
     
-            elif ctx.content.startswith('knockknock joke'):
+            elif ctx.content.startswith('::kk'):
                 await ctx.channel.send('Knock knock!')
-                #waiting for the user to send the message "who's there?" it will timeout in 60 seconds
                 res = await client.wait_for('message', check=lambda message: ctx.author == message.author)
                 if res.content.lower() == ("who's there?"):
 
-                    #opening knockjokes file and setting a list of each joke to knock_joke_list
+                   
                     with open("knockjokes.txt", "r", encoding="utf8") as f:
                         knock_joke_list = f.readlines()
-                    joke = random.choice(knock_joke_list).split("*") #choosing a random joke from the list and splitting that list into two sublists
+                    joke = random.choice(knock_joke_list).split("*") 
                     who = joke[0] #the who is the first element of the list
                     await ctx.channel.send(who)
 
-                    #waiting for user to send "{message that bot just sent} who?" it will timeout in 60 seconds
+
                     answer = await client.wait_for('message', check=lambda message: ctx.author == message.author)
                     if answer.content.lower() == (f"{who} who?".lower()):
                         await ctx.channel.send(joke[1]) #sending second element in joke sublist
+                    
                     
 
 client = myClient()
