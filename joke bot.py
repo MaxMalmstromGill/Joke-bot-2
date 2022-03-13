5fa3adbf5a69eea36011c4c22ae33812ad9579c2
MaxGill03 <98751815+MaxGill03@users.noreply.github.com>
2022-03-13 18:18:15 +0100

    Update joke bot.py

--- joke bot.py
+++ joke bot.py
@@ -2,19 +2,29 @@ import discord
 import requests
 import json
 import random
+import os
+
+client = discord.Client()
+
+
+ 
+
+
 
 
 class myClient(discord.Client):
     async def on_ready(self):
         print(f'Logged in as {client.user}'.format(client))
 
+   
+    
 
-
+        
     async def on_message(self, ctx):
             if ctx.author == client.user:
                 return
             elif ctx.content.startswith('::cn'):
-                await ctx.channel.send('Chuck Norris joke?')
+                await ctx.channel.send('Chuck Norris joke?, Yes/No')
                 chuck = await client.wait_for('message', check=lambda message: ctx.author == message.author)
                 if chuck.content.lower() == ('yes'):
                     async def get_joke(self):
@@ -24,8 +34,10 @@ class myClient(discord.Client):
                             return(joke['value'])
                     the_joke = await get_joke(self)
                     await ctx.channel.send(the_joke)
+                elif chuck.content.lower() == ('no'):
+                        await ctx.channel.send(':(')
             elif ctx.content.startswith('::ym'):
-                await ctx.channel.send('Yo momma joke?')
+                await ctx.channel.send('Yo momma joke?, Yes/No')
                 momma = await client.wait_for('message', check=lambda message: ctx.author == message.author)
                 if momma.content.lower() == ('yes'):
                     async def yomomma(self):
@@ -35,6 +47,18 @@ class myClient(discord.Client):
                             return(yo['joke'])
                     the_momma = await yomomma(self)
                     await ctx.channel.send(the_momma)
+                elif momma.content.lower() == ('no'):
+                        await ctx.channel.send(':(')
+            elif 'Joke Bot' or 'joke bot' or 'Joke bot' or 'joke Bot' in ctx.content:
+                await ctx.channel.send('If you need any help just send "::help" in the chat!')
+         
+           
+            elif ctx.content.startswith('::help'):
+                embed=discord.Embed(title="Commands for different type of jokes:", color=0x005275)
+                embed.add_field(name="Chuck Norris joke", value="::cn", inline=False)
+                embed.add_field(name="Knock Knock joke", value="::kk \n Who's there?  -  Who?", inline=False)
+                embed.add_field(name="Yo momma joke", value="::ym", inline=False)
+                await ctx.channel.send(embed=embed)
 
 
     
@@ -42,20 +66,19 @@ class myClient(discord.Client):
             elif ctx.content.startswith('::kk'):
                 await ctx.channel.send('Knock knock!')
                 res = await client.wait_for('message', check=lambda message: ctx.author == message.author)
-                if res.content.lower() == ("who's there?"):
+                if res.content.lower() == ("who's there?") or ("whos there") or ("whos there?") or ("who's there"):
 
                    
                     with open("knockjokes.txt", "r", encoding="utf8") as f:
                         knock_joke_list = f.readlines()
                     joke = random.choice(knock_joke_list).split("*") 
-                    who = joke[0]
+                    who = joke[0] 
                     await ctx.channel.send(who)
 
 
                     answer = await client.wait_for('message', check=lambda message: ctx.author == message.author)
-                    if answer.content.lower() == (f"{who} who?".lower()):
-                        await ctx.channel.send(joke[1]) 
-                    
+                    if answer.content.lower() == (f"{who} who?") or ("who?") or ("who"):
+                        await ctx.channel.send(joke[1])              
                     
 
 client = myClient()
